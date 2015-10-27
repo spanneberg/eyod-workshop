8938d88a4770d7426dfd31d3065ded554bf5c029
Bastian Spanneberg <bastian@spanneberg.com>
2015-10-27 11:09:24 +0100

    added hiera data and ci2-main node

--- puppet/environments/production/manifests/site.pp
+++ puppet/environments/production/manifests/site.pp
@@ -6,3 +6,7 @@ node 'ip-172-31-8-80' {
 node 'ip-172-31-29-191' {
   include ::roles::ci_master_and_artifact_repo 
 }
+
+node 'ip-172-31-2-196' {
+  include ::roles::ci_slave_and_sonarqube 
+}
--- puppet/modules/profiles/manifests/ci_slave.pp
+++ puppet/modules/profiles/manifests/ci_slave.pp
@@ -1,9 +1,5 @@
 class profiles::ci_slave {
   
-  class { '::jenkins::slave' :
-    masterurl => 'http://192.168.33.21:8080',
-    # ui_user => 'adminuser',
-    # ui_pass => 'adminpass',
-  }
+  include ::jenkins::slave
 
 }
